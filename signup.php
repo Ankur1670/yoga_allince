54617988ee84adb6e991de4ece00db9738224232
Ankur1670 <16sainiankur@gmail.com>
2024-10-17 01:23:07 +0530

    commit

--- signup.php
+++ signup.php
@@ -311,7 +311,7 @@
         const password = document.getElementById('loginPassword').value.trim();
 
         try {
-          const response = await fetch('http://localhost:5000/api/auth/login', {
+          const response = await fetch('/api/auth/login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
